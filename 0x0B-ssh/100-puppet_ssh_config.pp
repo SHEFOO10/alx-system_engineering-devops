ce9002fb69728cdebdbdd1ad823767bf8a010ffd
SHEFOO10 <445585913@g102019.moe.edu.eg>
2024-01-23 19:40:38 +0200

    again

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -3,14 +3,14 @@
 file_line { 'SSH_Private_key':
   ensure => 'present',
   path   => '/etc/ssh/ssh_config',
-  line   => '    IndentityFile ~/.ssh/school',
-  match  => '^[#]+[\s]*(?i)IdentityFile[\s]+~/.ssh/id_rsa$',
+  line   => 'IndentityFile ~/.ssh/school',
+  match  => '^#?IndentityFile',
 }
 
 
 file_line { 'Deny_Password_Auth':
   ensure => 'present',
   path   => '/etc/ssh/ssh_config',
-  line   => '    PasswordAuthentication no',
-  match  => '^[#]+[\s]*PasswordAuthentication[\s]+(yes|no)$',
+  line   => 'PasswordAuthentication no',
+  match  => '^#?PasswordAuthentication',
 }
