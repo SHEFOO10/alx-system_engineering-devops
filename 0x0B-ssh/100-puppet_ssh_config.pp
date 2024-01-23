937616a9fed6ce4eb43ad196eac1b12cc755ff0d
SHEFOO10 <445585913@g102019.moe.edu.eg>
2024-01-23 19:08:43 +0200

    4. Client configuration file (w/ Puppet)|

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -3,6 +3,7 @@
 include stdlib
 
 file_line { 'SSH Private key':
+  ensure             => 'present',
   path               => '/etc/ssh/ssh_config',
   line               => '    IndentityFile ~/.ssh/school',
   match              => '^[#]+[\s]*(?i)IdentityFile[\s]+~/.ssh/id_rsa$',
@@ -12,6 +13,7 @@ file_line { 'SSH Private key':
 
 
 file_line { 'Deny Password Auth':
+  ensure             => 'present',
   path               => '/etc/ssh/ssh_config',
   line               => '    PasswordAuthentication no',
   match              => '^[#]+[\s]*PasswordAuthentication[\s]+(yes|no)$',
