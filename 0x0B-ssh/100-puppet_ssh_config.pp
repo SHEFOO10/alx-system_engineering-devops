6793cb03350ffbbd82355c0cc70787b3e70f078d
SHEFOO10 <445585913@g102019.moe.edu.eg>
2024-01-23 19:49:16 +0200

    again

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,19 +1,17 @@
 # 4. Client configuration file (w/ Puppet)
 
+# configured to use the private key ~/.ssh/school
+file_line { 'configure_indentity':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => 'IndentityFile ~/.ssh/school',
+  match  => '^#?IndentityFile',
+}
 
-# Disable PasswordAuthentication in sshd_config
+# disable passowrd authentication
 file_line { 'disable_password_auth':
   ensure => 'present',
   path   => '/etc/ssh/ssh_config',
   line   => 'PasswordAuthentication no',
   match  => '^#?PasswordAuthentication',
-}
-
-# SSH client configuration must be configured to use the
-# private key ~/.ssh/school
-file_line { 'use_school_private_key':
-  ensure => 'present',
-  path   => '/etc/ssh/ssh_config',
-  line   => 'IdentityFile ~/.ssh/school',
-  match  => '^#?IdentityFile',
 }
