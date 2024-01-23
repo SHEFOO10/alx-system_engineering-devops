716194c0476e099647534a3767be21599840e283
SHEFOO10 <445585913@g102019.moe.edu.eg>
2024-01-23 19:45:06 +0200

    again

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,12 +1,5 @@
 # configure server private key
 
-# sets indentity key
-file_line { 'ssh_private_key':
-  ensure => 'present',
-  path   => '/etc/ssh/ssh_config',
-  line   => 'IndentityFile ~/.ssh/school',
-  match  => '^#?IndentityFile',
-}
 
 # disable passowrd authentication
 
@@ -15,4 +8,12 @@ file_line { 'Deny_Password_Auth':
   path   => '/etc/ssh/ssh_config',
   line   => 'PasswordAuthentication no',
   match  => '^#?PasswordAuthentication',
+}
+
+# sets indentity key
+file_line { 'ssh_private_key':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => 'IndentityFile ~/.ssh/school',
+  match  => '^#?IndentityFile',
 }
