5dcea9eb2d95abf538095c4219594651f10d0fbf
Sherif Hamdy <79402373+SHEFOO10@users.noreply.github.com>
2024-04-12 22:24:25 +0200

    Update 0-strace_is_your_friend.pp

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,3 +1,5 @@
+#Using strace  and then using Puppet to automate fix for 500 error on Apache
+
 exec { 'sed -i "s/.phpp/.php/" wp-settings.php':
   cwd  => '/var/www/html',
   path => ['/usr/bin', '/usr/sbin', '/bin',]
