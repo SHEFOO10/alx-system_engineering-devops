eacc752003b4514212a1df271d134ab58e57690d
SHEFOO10 <445585913@g102019.moe.edu.eg>
2024-04-18 16:32:42 +0200

    fix puppet warning

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,9 +1,9 @@
 # Modify the ulimit in the nginx file
 exec { 'replace increase ulimit from 15 to 1024':
-  command   => 'sed -i -E \'s/ULIMIT=".*"/ULIMIT="-n 1024"/\' /etc/default/nginx',
-  path      => ['/bin/'],
-  onlyif    => 'grep -q "^ULIMIT=\".*\"$" /etc/default/nginx', # Ensures the change only happens if needed
-  notify    => Exec['nginx_restart'], # Notify the restart exec when the sed command completes
+  command => 'sed -i -E \'s/ULIMIT=".*"/ULIMIT="-n 1024"/\' /etc/default/nginx',
+  path    => ['/bin/'],
+  onlyif  => 'grep -q "^ULIMIT=\".*\"$" /etc/default/nginx', # Ensures the change only happens if needed
+  notify  => Exec['nginx_restart'], # Notify the restart exec when the sed command completes
 }
 
 # Restart Nginx service
